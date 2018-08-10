ff544764bfdffd1687e21e90b4082c81ba8ef84f
Matt Ray <matthewhray@gmail.com>
2018-08-10 18:14:01 +1000

    Added test for policyfiles_loader
    Signed-off-by: Matt Ray <matthewhray@gmail.com>

---
+++ test/integration/policyfiles/default_test.rb
@@ -0,0 +1,16 @@
+# # encoding: utf-8
+
+# Inspec test for recipe managed-chef-server::policyfiles
+
+# check output of chef show-policy
+
+# verify the ChefDK
+describe command('chef') do
+  it { should exist }
+end
+
+describe command('chef show-policy -c /etc/opscode/managed/knife.rb') do
+  its ('stdout') { should match (/\* base:        bea04861be$/) }
+  its ('stdout') { should match (/\* beaglebone:  d99228eafe$/) }
+  its ('stdout') { should match (/\* macbookpro:  3e28786370$/) }
+end
