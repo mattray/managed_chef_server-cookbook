2b99ad89ab4c6d51f0cdccb33ee98e4302ef4779
Chris Gillings <chris.gillings@westpac.com.au>
2018-09-17 11:47:41 +1000

    Add chef-server-ctl backup script

---
+++ files/chef-server-backup.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+#
+# perform full backup of chef-server config (and data)
+
+# # Puts the initial backup in the /var/opt/chef-backup directory as a tar.gz file; move this backup to a new location for safe keeping
+# execute 'chef-server-ctl backup -y' do
+#   only_if { ::File.exist?("#{Chef::Config[:file_cache_path]}/chef-server-core.firstrun") }
+# end
+
+# set to "-c" to only backup config
+CONFIGONLY=""
+
+BACKDIR="/var/opt/chef-backup/"
+
+CHEFBIN="/bin/chef-server-ctl"
+
+if [ ! -x $CHEFBIN ]; then
+   echo "$CHEFBIN not found"
+   exit 1
+fi
+
+mkdir -p $BACKDIR
+if [ ! -d $BACKDIR ]; then
+   echo "Cannot create backup directory $BACKDIR"
+   exit 1
+fi
+
+# do full chef server backup
+
+$CHEFBIN backup -y $CONFIGONLY
+
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+   echo "Chef server backup failed with error $RESULT"
+   exit $RESULT
+fi
+
+BACKUPFILE=`ls -1tr $BACKDIR/*.tgz | tail -1`
+
+if [ ! -s $BACKUPFILE ]; then
+   echo "Cannot find backup file $BACKUPFILE"
+   exit 2
+fi
+
+ln -f -s $BACKUPFILE $BACKDIR/chef-backup-latest.tgz
+
+exit 0
+
+# that's all
--- recipes/backup.rb
+++ recipes/backup.rb
@@ -19,9 +19,34 @@ end
 
 # schedule backups on a recurring cron job. Refer to the README for further customization
 cron 'knife ec backup' do
-  environment ({ 'PWD' => bdir })
+  # environment ({ 'PWD' => bdir })
   command command
   minute '*/5'
   hour '*'
+  day '*'
+end
+
+#####################################
+
+cdir = '/var/opt/chef-backup/'
+cmd = '/root/chef-server-backup.sh'
+
+directory cdir
+
+# shell script for backups
+cookbook_file cmd do
+  mode '0700'
+  source 'chef-server-backup.sh'
+  owner 'root'
+  group 'root'
+  action :create
+end
+
+# schedule backups on a recurring cron job. Refer to the README for further customization
+cron 'chef-server-ctl backup' do
+  # environment ({ 'PWD' => cdir })
+  command cmd
+  minute '27'
+  hour '4'
   day '*'
 end
