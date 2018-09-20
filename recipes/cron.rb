bd114023355c037b42fe9ccbe3d93040007b1b53
Matt Ray <matthewhray@gmail.com>
2018-09-20 23:25:53 +1000

    Placeholder recipe for cron work
    Signed-off-by: Matt Ray <matthewhray@gmail.com>

---
+++ recipes/cron.rb
@@ -0,0 +1,7 @@
+#
+# Cookbook:: managed-chef-server
+# Recipe:: cron
+#
+
+# install the chef-client for the chef-server as a cron-managed chef-zero
+# useful for the root chef-server managing other chef-servers
