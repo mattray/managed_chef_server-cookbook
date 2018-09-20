d58968e567c35626a0f0e650f546fc4505b979cf
Matt Ray <matthewhray@gmail.com>
2018-09-20 23:25:21 +1000

    Clarify variable names
    Signed-off-by: Matt Ray <matthewhray@gmail.com>

--- recipes/backup.rb
+++ recipes/backup.rb
@@ -3,16 +3,16 @@
 # Recipe:: backup
 #
 
-bdir = node['mcs']['backup']['dir']
-command = "#{bdir}/backup.sh"
+backupdir = node['mcs']['backup']['dir']
+command = "#{backupdir}/backup.sh"
 
-directory bdir
+directory backupdir
 
 # shell script for backups
 file command do
   mode '0700'
   content "#/bin/sh
-cd #{bdir}
+cd #{backupdir}
 /opt/opscode/embedded/bin/knife ec backup --with-key-sql --with-user-sql -c /etc/opscode/pivotal.rb backup > backup.log 2>&1
 cd backup
 tar -czf ../#{node['mcs']['backup']['prefix']}`date +%Y%m%d%H%M`.tgz *
@@ -22,7 +22,7 @@ end
 
 # schedule backups on a recurring cron job. Refer to the README for further customization
 cron 'knife ec backup' do
-  environment ({ 'PWD' => bdir })
+  environment ({ 'PWD' => backupdir })
   command command
   minute '*/5'
   hour '*'
