a48e18cbd62a7c7984327de65a0e999dfaab5c2d
Matt Ray <matthewhray@gmail.com>
2018-09-20 23:27:03 +1000

    get the policyname from the lockfile
    Signed-off-by: Matt Ray <matthewhray@gmail.com>

--- recipes/policyfile_loader.rb
+++ recipes/policyfile_loader.rb
@@ -12,26 +12,27 @@ node.override['chefdk']['channel'] = :stable
 # chef export base.rb . -a # generates base-VERSION.tgz
 # chef push-archive POLICY_GROUP base-VERSION.tgz -c config_file --debug
 
-pdir = node['mcs']['policyfile']['dir']
+policydir = node['mcs']['policyfile']['dir']
 
 # find the local policyfiles
-Dir.foreach(pdir) do |pfile|
+Dir.foreach(policydir) do |pfile|
   next unless pfile.end_with?('.lock.json')
 
   # parse the filename, drop the .lock.json
   policy = pfile.sub(/\.lock\.json$/, '')
 
   # parse the JSON file, get the revision ID
-  plock = JSON.parse(File.read(pdir + '/' + pfile))
+  plock = JSON.parse(File.read(policydir + '/' + pfile))
   revision = plock['revision_id']
+  policyname = plock['name']
   short_rev = revision[0, 9]
   # match the right policyfile archive
-  filename = pdir + '/' + policy + '-' + revision + '.tgz'
-  kniferb = node['mcs']['managed_user']['dir'] + '/config.rb'
+  filename = policydir + '/' + policy + '-' + revision + '.tgz'
+  configrb = node['mcs']['managed_user']['dir'] + '/config.rb'
 
   # push the archive to the policygroup, currently the name of the policyfile
-  execute "chef push-archive #{policy} #{policy}-#{revision}.tgz" do
-    command "chef push-archive #{policy} #{filename} -c #{kniferb}"
+  execute "chef push-archive #{policyname} #{filename}-#{revision}.tgz" do
+    command "chef push-archive #{policyname} #{filename} -c #{configrb}"
     # add a guard to check if chef show-policy
     not_if "chef show-policy #{policy} -c #{kniferb} | grep '* #{policy}' | grep #{short_rev}"
   end
