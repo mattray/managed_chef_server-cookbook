27c0b6c6b7bb3f34994079e199f2700d3d448fdf
ChrisG <41408630+chrisg-fastlane@users.noreply.github.com>
2018-09-27 13:55:16 +1000

    Archive name is no longer derived from the lock file name
    Now we derive the policy archive filename from the name in the lock file, not the lock filename

---
+++ policyfile_loader.rb
@@ -0,0 +1,36 @@
+#
+# Cookbook:: managed-chef-server
+# Recipe:: policyfile_loader
+#
+
+# need the ChefDK for the 'chef' command
+include_recipe 'chefdk::default'
+node.override['chefdk']['channel'] = :stable
+
+# loads all of the policyfile lock files in a directory into the Chef server
+# chef install base.rb     # generates base.lock.json
+# chef export base.rb . -a # generates base-VERSION.tgz
+# chef push-archive POLICY_GROUP base-VERSION.tgz -c config_file --debug
+
+policydir = node['mcs']['policyfile']['dir']
+
+# find the local policyfiles
+Dir.foreach(policydir) do |pfile|
+  next unless pfile.end_with?('.lock.json')
+
+  # parse the JSON file, get the revision ID
+  plock = JSON.parse(File.read(policydir + '/' + pfile))
+  revision = plock['revision_id']
+  policyname = plock['name']
+  short_rev = revision[0, 9]
+  # match the right policyfile archive based on name in lock file
+  filename = policydir + '/' + policyname + '-' + revision + '.tgz'
+  configrb = node['mcs']['managed_user']['dir'] + '/config.rb'
+
+  # push the archive to the policygroup under the policy name
+  execute "chef push-archive #{policyname} #{filename}" do
+    command "chef push-archive #{policyname} #{filename} -c #{configrb}"
+    # add a guard to check if chef show-policy indicates the policy is already installed
+    not_if "chef show-policy #{policyname} -c #{configrb} | grep '* #{policyname}' | grep #{short_rev}"
+  end
+end
