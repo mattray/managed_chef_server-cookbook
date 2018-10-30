a74cc96f6914b7a4ba084b4b3bc65d04866b9e4a
Matt Ray <matthewhray@gmail.com>
2018-10-30 15:19:38 +1100

    Catch nil directory
    Signed-off-by: Matt Ray <matthewhray@gmail.com>

--- recipes/policyfile_loader.rb
+++ recipes/policyfile_loader.rb
@@ -16,21 +16,23 @@ policydir = node['mcs']['policyfile']['dir']
 configrb = node['mcs']['managed_user']['dir'] + '/config.rb'
 
 # find the local policyfiles
-Dir.foreach(policydir) do |pfile|
-  next unless pfile.end_with?('.lock.json')
+unless policydir.nil?
+  Dir.foreach(policydir) do |pfile|
+    next unless pfile.end_with?('.lock.json')
 
-  # parse the JSON file, get the revision ID
-  plock = JSON.parse(File.read(policydir + '/' + pfile))
-  revision = plock['revision_id']
-  policyname = plock['name']
-  short_rev = revision[0, 9]
-  # match the right policyfile archive based on name in lock file
-  filename = policydir + '/' + policyname + '-' + revision + '.tgz'
+    # parse the JSON file, get the revision ID
+    plock = JSON.parse(File.read(policydir + '/' + pfile))
+    revision = plock['revision_id']
+    policyname = plock['name']
+    short_rev = revision[0, 9]
+    # match the right policyfile archive based on name in lock file
+    filename = policydir + '/' + policyname + '-' + revision + '.tgz'
 
-  # push the archive to the policygroup under the policy name
-  execute "chef push-archive #{policyname} #{filename}" do
-    command "chef push-archive #{policyname} #{filename} -c #{configrb}"
-    # add a guard to check if chef show-policy indicates the policy is already installed
-    not_if "chef show-policy #{policyname} -c #{configrb} | grep '* #{policyname}' | grep #{short_rev}"
+    # push the archive to the policygroup under the policy name
+    execute "chef push-archive #{policyname} #{filename}" do
+      command "chef push-archive #{policyname} #{filename} -c #{configrb}"
+      # add a guard to check if chef show-policy indicates the policy is already installed
+      not_if "chef show-policy #{policyname} -c #{configrb} | grep '* #{policyname}' | grep #{short_rev}"
+    end
   end
 end
