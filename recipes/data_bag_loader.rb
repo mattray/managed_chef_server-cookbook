8d0af521b6608fe7a1aba634854016b8c53ceda2
Matt Ray <matthewhray@gmail.com>
2019-01-07 17:27:30 -0800

    Properly manage creating, pruning, and syncing data bags with files
    Signed-off-by: Matt Ray <matthewhray@gmail.com>

--- recipes/data_bag_loader.rb
+++ recipes/data_bag_loader.rb
@@ -11,17 +11,13 @@ prune = node['mcs']['data_bags']['prune']
 existing_dbags = {}
 list = `knife data bag list -c #{configrb}`.split
 list.each do |dbag|
-  puts dbag
   existing_dbags[dbag] = {}
   items = `knife data bag show #{dbag} -c #{configrb}`.split
   items.each do |item|
-    puts "EXISTING-#{dbag}::#{item}"
     content = `knife data bag show #{dbag} #{item} -c #{configrb} --format json`
-    existing_dbags[dbag][item] = content
+    existing_dbags[dbag][item] = eval(content)
   end
 end
-
-puts "EXISTING-"+existing_dbags
 
 # get list of data bags to manage
 file_dbags = {}
@@ -29,23 +25,17 @@ file_dbags_files = {} # file associated with the data bag items
 unless dbdir.nil?
   Dir.foreach(dbdir) do |dbag|
     next if ['.', '..'].member?(dbag)
-    puts dbag
     file_dbags[dbag] = {}
     file_dbags_files[dbag] = {}
     bagdir = dbdir + '/' + dbag
     Dir.foreach(bagdir) do |itemfile| # get items for each
       next if ['.', '..'].member?(itemfile)
-      puts "FILE-#{dbag}::#{itemfile}"
-      # read in JSON for comparison?
       item = JSON.parse(File.read(bagdir + '/' + itemfile))
       file_dbags[dbag][item['id']] = item
       file_dbags_files[dbag][item['id']] = itemfile
-      # convert hash to data bag item?
     end
   end
 end
-
-puts "FILE-"+file_dbags
 
 # if we are pruning we are deleting unmanaged data bags and items
 if prune
@@ -53,7 +43,7 @@ if prune
   deleted_dbags = existing_dbags.keys - file_dbags.keys
   deleted_dbags.each do |dbag|
     execute "knife data bag delete #{dbag}" do
-      command "knife data bag delete #{dbag} -c #{configrb}"
+      command "knife data bag delete #{dbag} -y -c #{configrb}"
     end
   end
   # existing items not in new, those are deletes
@@ -62,7 +52,7 @@ if prune
     deleted_items = existing_dbags[dbag].keys - file_dbags[dbag].keys
     deleted_items.each do |item|
       execute "knife data bag delete #{dbag} #{item}" do
-        command "knife data bag delete #{dbag} #{item} -c #{configrb}"
+        command "knife data bag delete #{dbag} #{item} -y -c #{configrb}"
       end
     end
   end
@@ -77,12 +67,17 @@ new_dbags.each do |dbag|
 end
 # items new or changed from existing, those are from file
 shared_dbags = file_dbags.keys & existing_dbags.keys
+shared_dbags += new_dbags # don't forget the new ones
 shared_dbags.each do |dbag|
   file_dbags[dbag].keys.each do |item|
-    # can you knife data bag create item with JSON?
-    execute "knife data bag from file #{dbag} #{file_dbags_files[item]}" do
-      command "knife data bag from file #{dbag} #{file_dbags_files[item]} -c #{configrb}"
-      cwd dbdir
+    # compare existing data bag items with those in the file
+    unless existing_dbags.key?(dbag) &&
+        existing_dbags[dbag].key?(item) &&
+        file_dbags[dbag][item].to_json.eql?(existing_dbags[dbag][item].to_json)
+      execute "knife data bag from file #{dbag} #{dbag}/#{file_dbags_files[dbag][item]}" do
+        command "knife data bag from file #{dbag} #{dbag}/#{file_dbags_files[dbag][item]} -c #{configrb}"
+        cwd dbdir
+      end
     end
   end
 end
