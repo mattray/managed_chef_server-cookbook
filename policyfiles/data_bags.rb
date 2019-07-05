94c5197fc3c1359205645e6de2d1b907e7e86e27
Matt Ray <matthewhray@gmail.com>
2019-07-05 14:54:25 +1000

    Initial data bag load with pruning working for removed data bags.
    Documented resources inline a bit to explain the usage of the MD5 files.
    Essentially knife API calls were way too slow for pruning, tracking
    state with a file proved much faster, especially on subsequent runs.
    
    Signed-off-by: Matt Ray <matthewhray@gmail.com>

--- policyfiles/data_bags.rb
+++ policyfiles/data_bags.rb
@@ -4,7 +4,7 @@ include_policy 'base', path: './base.lock.json'
 
 run_list 'managed-chef-server::data_bag_loader'
 
-# override['mcs']['restore']['file'] = '/backups/chef-server-backup-201811110055.tgz' # pre-baked with 2 data bag items existing
 override['mcs']['data_bags']['dir'] = '/backups/data_bags'
+override['mcs']['data_bags']['prune'] = true
 
 override['mcs']['skip_test'] = true
--- resources/data_bag_loader.rb
+++ resources/data_bag_loader.rb
@@ -2,31 +2,60 @@ resource_name :data_bag_loader
 
 property :directory, String, name_property: true, required: true
 property :organization, String, required: true
-# property :prune, Boolean, default: false
+property :prune, [true, false], default: false
 
 action :load do
   data_bag_dir = new_resource.directory
   organization = new_resource.organization
-  # prune = new_resource.prune
+  prune = new_resource.prune
+
+  configrb = "/etc/opscode/managed/#{organization}/config.rb"
+
+  # find the data bags by the directory names
+  dir_data_bags = Dir.entries(data_bag_dir) - ['.', '..']
 
   # get list of data bags to manage
-  Dir.foreach(data_bag_dir) do |data_bag|
-    next if ['.', '..'].member?(data_bag)
-
+  dir_data_bags.each do |data_bag|
+    # create if missing
     data_bag "#{organization}:#{data_bag}" do
       organization organization
       data_bag data_bag
       action :create
     end
 
-    Dir.foreach(data_bag_dir + '/' + data_bag) do |item_json| # get items for each
-      next if ['.', '..'].member?(item_json)
+    data_bag_files = Dir.entries(data_bag_dir + '/' + data_bag) - ['.', '..']
 
+    data_bag_files.sort.each do |item_json| # get items for each
       data_bag "#{organization}:#{data_bag}:#{data_bag_dir}/#{data_bag}/#{item_json}" do
         organization organization
         data_bag data_bag
-        item_json "#{data_bag_dir}/#{data_bag}/#{item_json}"
+        item "#{data_bag_dir}/#{data_bag}/#{item_json}"
         action :item_create
+      end
+
+    end
+
+    # if prune
+    #   data_bag "#{organization}:#{data_bag}:#{data_bag_dir}/#{data_bag}/#{item_json}" do
+    #     organization organization
+    #     data_bag data_bag
+    #     item "#{data_bag_dir}/#{data_bag}/#{item_json}"
+    #     action :item_delete
+    #   end
+    # end
+
+  end
+
+  # if prune is true, get the existing data bags from the server
+  if prune
+    server_data_bags = shell_out("knife data bag list -c #{configrb}").stdout.split
+    # if on server, but not in the directory, remove them
+    (server_data_bags - dir_data_bags).each do |prune_data_bag|
+
+      data_bag "#{organization}:#{prune_data_bag}" do
+        organization organization
+        data_bag prune_data_bag
+        action :prune
       end
 
     end
