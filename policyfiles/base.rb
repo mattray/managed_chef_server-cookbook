0360bf3805c22109a1af2a0b6e8f8ee363968bff
Matt Ray <github@mattray.dev>
2020-02-03 12:00:46 +1100

    latest release

--- policyfiles/base.rb
+++ policyfiles/base.rb
@@ -8,7 +8,9 @@ run_list 'managed-chef-server::default', 'managed-chef-server::managed_organizat
 
 override['mcs']['managed_user']['email'] = 'test@foo.com'
 
+override['chef-server']['accept_license'] = true
+
 # override['chefdk']['package_source'] = '/backups/chefdk-3.11.3-1.el7.x86_64.rpm'
 # override['chefdk']['package_source'] = '/backups/chefdk-4.2.0-1.el7.x86_64.rpm'
 # override['chef-server']['package_source'] = '/backups/chef-server-core-12.19.31-1.el7.x86_64.rpm'
-override['chef-server']['version'] = '12.19.31'
+# override['chef-server']['version'] = '12.19.31'
