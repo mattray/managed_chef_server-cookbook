4c2840c5097d046a51913b81ced2c757fb15aedb
Matt Ray <github@mattray.dev>
2020-02-03 12:01:03 +1100

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,14 +4,14 @@ maintainer_email 'matt@chef.io'
 license 'Apache-2.0'
 description 'Installs and configures a Chef server'
 long_description 'Installs and configures a Chef server'
-version '0.15.1'
+version '0.16.0'
 chef_version '>= 14' if respond_to?(:chef_version)
 
 supports 'redhat'
 supports 'centos'
 
 depends 'chef-server', '~> 5.5.2'
-depends 'chef-ingredient', '~> 3.1.1'
+depends 'chef-ingredient', '~> 3.1.2'
 
 source_url 'https://github.com/mattray/managed-chef-server'
 issues_url 'https://github.com/mattray/managed-chef-server/issues'
