4ca6f78c0c846335513a5640fc2ce35a9392b9fc
Matt Ray <github@mattray.dev>
2020-02-03 12:09:09 +1100

    cookstyle cleanup

--- metadata.rb
+++ metadata.rb
@@ -3,9 +3,8 @@ maintainer 'Matt Ray'
 maintainer_email 'matt@chef.io'
 license 'Apache-2.0'
 description 'Installs and configures a Chef server'
-long_description 'Installs and configures a Chef server'
 version '0.16.0'
-chef_version '>= 14' if respond_to?(:chef_version)
+chef_version '>= 14'
 
 supports 'redhat'
 supports 'centos'
