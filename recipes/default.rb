a9270360e895734b5e09e9e1185ea070c350a4da
Matt Ray <github@mattray.dev>
2020-02-06 09:33:41 +1100

    Write out a templatized accepted license for Chef Infra Server

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,9 +3,18 @@
 # Recipe:: default
 #
 
-file '/etc/chef/accepted_licenses/chef_infra_server' do
-  only_if { node['chef-server']['accept_license'] }
+# Chef Server 13 requires license acceptance
+directory '/etc/chef/accepted_licenses/' do
+  recursive true
 end
+
+ template '/etc/chef/accepted_licenses/chef_infra_server' do
+   source 'chef_infra_server.erb'
+   mode '0400'
+   variables(time: Time.now)
+   not_if { ::File.exist?('/etc/chef/accepted_licenses/chef_infra_server') }
+   only_if { node['chef-server']['accept_license'] }
+ end
 
 # need the ChefDK for the 'berks' and 'chef' commands
 include_recipe 'managed_chef_server::_chefdk'
