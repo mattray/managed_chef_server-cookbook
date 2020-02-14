02f66cbf363cc6d0b1f7009cc6e1ca8719db0310
Matt Ray <github@mattray.dev>
2020-02-15 10:57:25 +1100

    install Chef Workstation and correct symlink

---
+++ recipes/_workstation.rb
@@ -0,0 +1,14 @@
+# need the Chef Workstation for the 'chef' command
+chef_ingredient 'chef-workstation' do
+  action :install
+  version node['chef-workstation']['version']
+  channel node['chef-workstation']['channel']
+  package_source node['chef-workstation']['package_source']
+end.run_action(:install)
+
+# this symlink confuses chef-workstation and chef client packages and may
+# downgrade/upgrade the chef-client version unintentionally
+# https://github.com/mattray/managed_chef_server-cookbook/issues/36
+link '/usr/bin/chef-client' do
+  to '/opt/chef/bin/chef-client'
+end
