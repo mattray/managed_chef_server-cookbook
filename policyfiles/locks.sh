724e9d12f304a4a35988f151255330b07fca1e27
Matt Ray <github@mattray.dev>
2020-02-15 10:57:53 +1100

    no push-jobs-server policy

--- policyfiles/locks.sh
+++ policyfiles/locks.sh
@@ -1,13 +1,15 @@
+set -e
+rm -rf ~/.chefdk/cache
 rm -rf *json
+
 chef install base.rb
 chef install default.rb
 
 chef install backup.rb
 chef install cron.rb
+chef install policyfile.rb
 chef install data_bags.rb
 chef install legacy.rb
-chef install policyfile.rb
-chef install push-jobs-server.rb
 chef install restore.rb
 
 chef install everything.rb
