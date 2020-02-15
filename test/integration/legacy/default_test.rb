3846130741ec19334db5ee455cac851ced87dcba
Matt Ray <github@mattray.dev>
2020-02-15 12:19:05 +1100

    updated cookbook versions because of Berkshelf drift

--- test/integration/legacy/default_test.rb
+++ test/integration/legacy/default_test.rb
@@ -19,12 +19,13 @@ describe command('knife role list -c /etc/opscode/managed/chef_managed_org/confi
   its('stdout') { should_not match /^lab-environment$/ }
 end
 
+# these are going to drift because of Berkshelf
 describe command('knife cookbook list -c /etc/opscode/managed/chef_managed_org/config.rb') do
-  its('stdout') { should match /^apt\s+7.1.1$/ }
+  its('stdout') { should match /^apt\s+7.2.0$/ }
   its('stdout') { should match /^chef-client\s+11.0.3$/ }
-  its('stdout') { should match /^cron\s+6.2.1$/ }
+  its('stdout') { should match /^cron\s+6.2.2$/ }
   its('stdout') { should match /^iptables\s+4.3.4$/ }
-  its('stdout') { should match /^logrotate\s+2.2.0$/ }
+  its('stdout') { should match /^logrotate\s+2.2.2$/ }
   its('stdout') { should match /^mattray\s+0.8.0$/ }
   its('stdout') { should match /^ntp\s+3.6.0/ }
   its('stdout') { should match /^sudo\s+5.5.0/ }
