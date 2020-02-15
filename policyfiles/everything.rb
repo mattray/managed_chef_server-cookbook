9946b8a944a4776223a0b94b84190914a7481bb8
Matt Ray <github@mattray.dev>
2020-02-15 15:09:26 +1100

    add data bags and remove unused maintenance recipe

--- policyfiles/everything.rb
+++ policyfiles/everything.rb
@@ -2,13 +2,14 @@ name 'everything'
 
 include_policy 'base', path: './base.lock.json'
 
-run_list 'managed_chef_server::legacy_loader', 'managed_chef_server::policyfile_loader', 'managed_chef_server::maintenance', 'managed_chef_server::backup'
+run_list 'managed_chef_server::data_bag_loader', 'managed_chef_server::legacy_loader', 'managed_chef_server::policyfile_loader', 'managed_chef_server::backup'
 
 # every 5 minutes for testing
 override['mcs']['backup']['cron']['minute'] = '*/5'
 override['mcs']['backup']['cron']['hour'] = '*'
-override['mcs']['data_bags']['dir'] = '/backups/data_bags'
-override['mcs']['policyfile']['dir'] = '/backups/policyfiles'
+
 override['mcs']['cookbooks']['dir'] = '/backups/cookbooks'
+override['mcs']['data_bags']['dir'] = '/backups/data_bags'
 override['mcs']['environments']['dir'] = '/backups/environments'
+override['mcs']['policyfile']['dir'] = '/backups/policyfiles'
 override['mcs']['roles']['dir'] = '/backups/roles'
