a340581beff6bade9027771161f749ac5d45c937
Matt Ray <github@mattray.dev>
2020-02-18 10:21:01 +1100

    Cookstyle cleanup

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,13 +8,13 @@ directory '/etc/chef/accepted_licenses/' do
   recursive true
 end
 
- template '/etc/chef/accepted_licenses/chef_infra_server' do
-   source 'chef_infra_server.erb'
-   mode '0400'
-   variables(time: Time.now)
-   not_if { ::File.exist?('/etc/chef/accepted_licenses/chef_infra_server') }
-   only_if { node['chef-server']['accept_license'] }
- end
+template '/etc/chef/accepted_licenses/chef_infra_server' do
+  source 'chef_infra_server.erb'
+  mode '0400'
+  variables(time: Time.now)
+  not_if { ::File.exist?('/etc/chef/accepted_licenses/chef_infra_server') }
+  only_if { node['chef-server']['accept_license'] }
+end
 
 # need the ChefDK for the 'berks' and 'chef' commands
 include_recipe 'managed_chef_server::_workstation'
