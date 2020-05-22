8f7e530424a1e6e1092d7b94211c6627629dbec1
Matt Ray <github@mattray.dev>
2020-05-23 09:24:58 +1000

    put the certs in the right place

--- test/integration/test_cookbook/recipes/default.rb
+++ test/integration/test_cookbook/recipes/default.rb
@@ -1,12 +1,14 @@
+directory "#{Chef::Config.etc_chef_dir}/trusted_certs"
+
 # self-signed cert for internal A2 testing
-cookbook_file '/etc/chef/trusted_certs/inez_bottlebru_sh.crt' do
+cookbook_file "#{Chef::Config.etc_chef_dir}/trusted_certs/inez_bottlebru_sh.crt" do
   sensitive true
   source 'inez_bottlebru_sh.crt'
   mode '0644'
 end
 
 # self-signed cert for internal Chef Infra Server
-cookbook_file '/etc/chef/trusted_certs/ndnd_bottlebru_sh.crt' do
+cookbook_file "#{Chef::Config.etc_chef_dir}/trusted_certs/ndnd_bottlebru_sh.crt" do
   sensitive true
   source 'ndnd_bottlebru_sh.crt'
   mode '0644'
