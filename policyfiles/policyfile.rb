dafdd01f2c2a95c7b7b44df06fc20d597d3452ac
Matt Ray <github@mattray.dev>
2020-05-23 07:55:28 +1000

    Removed policyfiles in favor of single Policyfile with named run lists
    Signed-off-by: Matt Ray <github@mattray.dev>

--- policyfiles/policyfile.rb
+++
@@ -1,9 +0,0 @@
-name 'policyfile'
-
-include_policy 'default', path: './default.lock.json'
-
-run_list 'managed_chef_server::policyfile_loader'
-
-override['mcs']['policyfile']['dir'] = '/backups/policyfiles'
-
-override['mcs']['skip_test'] = true
