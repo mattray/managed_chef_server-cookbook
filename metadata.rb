60d108e4eee9951e77d36aca9034b16ed65776a3
Matt Ray <github@mattray.dev>
2021-04-02 17:19:29 +1100

    initial Chef Server 14 work, updating tests and dependencies
    Signed-off-by: Matt Ray <github@mattray.dev>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Matt Ray'
 maintainer_email 'matt@chef.io'
 license 'Apache-2.0'
 description 'Installs and configures a Chef server'
-version '0.19.1'
+version '0.20.0'
 chef_version '>= 15'
 
 supports 'redhat'
@@ -11,7 +11,7 @@ supports 'centos'
 supports 'debian'
 
 depends 'chef-server', '~> 5.6'
-depends 'chef-ingredient', '~> 3.2'
+depends 'chef-ingredient', '~> 3.3'
 
 source_url 'https://github.com/mattray/managed_chef_server-cookbook'
 issues_url 'https://github.com/mattray/managed_chef_server-cookbook/issues'
