f0cb545ac8923440ab1464eafdf79f371dc07060
Matt Ray <github@mattray.dev>
2021-04-29 16:38:11 +1000

    Chef Server 14.3.14 tests and Centos 8

--- test/integration/upgrade14/default_test.rb
+++ test/integration/upgrade14/default_test.rb
@@ -3,19 +3,19 @@
 if os.debian?
   describe file '/tmp/kitchen/cache/managed_chef_server.upgraded' do
     it { should exist }
-    its('content') { should match /^chef-server-core_14.2.2-1_amd64.deb$/ }
+    its('content') { should match /^chef-server-core_14.3.14-1_amd64.deb$/ }
   end
   describe command('apt list --installed | grep chef-server-core') do
     its('stdout') { should match /^chef-server-core/ }
-    its('stdout') { should match /14.2.2-1/ }
+    its('stdout') { should match /14.3.14-1/ }
     its('stdout') { should match /amd64/ }
   end
 elsif os.redhat?
   describe file '/tmp/kitchen/cache/managed_chef_server.upgraded' do
     it { should exist }
-    its('content') { should match /^chef-server-core-14.2.2-1.el7.x86_64.rpm$/ }
+    its('content') { should match /^chef-server-core-14.3.14-1.el7.x86_64.rpm$/ }
   end
   describe command('rpm -aq | grep chef-server-core') do
-    its('stdout') { should match /^chef-server-core-14.2.2-1.el7.x86_64$/ }
+    its('stdout') { should match /^chef-server-core-14.3.14-1.el7.x86_64$/ }
   end
 end
